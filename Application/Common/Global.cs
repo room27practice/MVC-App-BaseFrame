15bb74d54d4983ce30f7d2ae83edbe5ede274860
eng. Hristov <h@abv.bg>
2022-01-28 10:30:05 +0200

    Implemented better Naming of tabs, and CSS-mapping.

--- Application/Common/Global.cs
+++ Application/Common/Global.cs
@@ -1,9 +1,21 @@
-﻿namespace Common
+﻿using System.Collections.Generic;
+
+namespace Common
 {
     public static class Global
     {
         public static string ApplicationName { get; } = "MyApp";
 
         public static string AdminRoleName { get; } = "Admin";
+
+        public static AppTitles Titles = new AppTitles { Home = "Home Page", Privacy = "Privacy", Products = "All Products", ProductDetails = "Details of Product" };
+    }
+
+    public class AppTitles
+    {
+        public string Home { get; set; }
+        public string Privacy { get; set; }
+        public string Products { get; set; }
+        public string ProductDetails { get; set; }
     }
 }
